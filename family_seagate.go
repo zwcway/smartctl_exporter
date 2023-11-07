98897cd1887664a688e95f47f701007fc6c4ddb9
zwcway <zwcway@qq.com>
2023-11-07 09:02:22 +0000

    fixed bug

--- family_seagate.go
+++ family_seagate.go
@@ -9,8 +9,11 @@ func (s *Seagate) DeviceAttributeRaw(id string, name string, val float64) float6
 	switch id {
 	case "1", "7":
 		v := int64(val)
-		v >>= 47
-		v &= 0b111111
+		// raw48:54
+		// 5 is 47-40bit
+		// 4 is 39-32bit
+		// 54 is Little-Endian
+		v >>= 32
 		return float64(v)
 	}
 
